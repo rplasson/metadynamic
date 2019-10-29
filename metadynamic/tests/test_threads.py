a793d2047be1cfcf11b982164a2e1e36b2b38caa
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2019-10-29 10:17:17 +0100

    Better test logging... More bug to fix For sure: bad descriptions of activated reactions Likely: bugs in  reaction processing (some runs finish by decrementing 0 pop)

--- metadynamic/tests/test_threads.py
+++ metadynamic/tests/test_threads.py
@@ -2,9 +2,14 @@ from metadynamic import System
 
 
 def test_system() -> None:
-    syst = System({"a": 1000, "A": 2000}, consts={"P": 10.0, "A": 10.0, "H": 1.0, "E": 1.0, "a": 1.0, "d": 1.0, "R": 0.1}, dropreac=False)
+    syst = System(
+        {"a": 1000, "A": 2000},
+        consts={"P": 10.0, "A": 10.0, "H": 1.0, "E": 1.0, "a": 1.0, "d": 1.0, "R": 0.1},
+        dropreac=False,
+        logfile="testlog/test-thread.log",
+    )
     syst.set_param(conc=1.0, tend=1.0, tstep=0.05)
     syst.set_param(save=["a", "A", "aa", "aA", "Aa", "AA"])
     res = syst.multirun(4)
-    res.table().to_csv("testlog/test-thread.log", sep=",")
+    res.table().to_csv("testlog/test-thread-result.txt", sep=",")
     print(res.the_end())
