1084ec3541cc1297a0f40a1808ce04804a34c53a
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2019-12-10 08:56:47 +0100

    Updated profiling tests

--- launch_profile.sh
+++ launch_profile.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export name=testlog/$1-$HOSTNAME-$(date '+%Y-%m-%d')
 time python3 -OO -m cProfile -o $name.prof $1.py --log $name.log $name.txt 
-gprof2dot --colour-nodes-by-selftime -n 0.5 -e 0.1 --skew=0.05 -f pstats $name.prof -o $name.dot
+gprof2dot --colour-nodes-by-selftime -n 1 -e 0.5 --skew=0.05 -f pstats $name.prof -o $name.dot
 dot -Tpdf $name.dot -o $name.pdf
 
 
---
+++ testrun-gc.py
@@ -0,0 +1,19 @@
+from metadynamic import System
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Test multithread.")
+
+parser.add_argument("output", type=str, help="output file (csv)")
+parser.add_argument("--log", metavar="logfile", type=str, nargs="?", help="log file")
+parser.add_argument(
+    "--level", metavar="loglevel", type=str, nargs="?", help="log level", default="INFO"
+)
+
+args = parser.parse_args()
+
+syst = System("test-gc.json", logfile=args.log, loglevel=args.level)
+syst.runparam.set_param(dropmode="drop")
+res = syst.run()
+
+res.table().to_csv(args.output, sep=",")
+print(res.end())
