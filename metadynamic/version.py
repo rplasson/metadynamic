04730a57a071e3ae8d4e8302cdebcd346e70ae73
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-01-09 11:57:59 +0100

    Switch to new devel version

--- metadynamic/version.py
+++ metadynamic/version.py
@@ -18,4 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.3.6-devel'
+__version__ = '0.3.7-devel'
