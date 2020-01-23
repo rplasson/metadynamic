cc0fc0ece404d9d9a9ba027c054fca35d4f9c5a2
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-01-23 16:51:25 +0100

    Added convertor script

---
+++ json2dot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 by Raphaël Plasson
+#
+# This file is part of metadynamic
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from metadynamic.json2dot import Json2dot
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Test multithread.")
+
+parser.add_argument("file_in", type=str, help="input file (json)")
+parser.add_argument("file_out", type=str, help="output file (dot)")
+
+args = parser.parse_args()
+
+Json2dot(args.file_in).write(args.file_out)
