687fce16b8ddbf30a96260c99e041614b4b1062d
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-02-12 19:08:29 +0100

    Added hdf5 generation in tests

--- metadynamic/tests/test_simplesyst_dropreac.py
+++ metadynamic/tests/test_simplesyst_dropreac.py
@@ -6,6 +6,7 @@ def test_system() -> None:
         "metadynamic/tests/simplesyst.json", logfile="testlog/test-dropreac.log"
     )
     syst.set_param(dropmode="drop")
+    syst.set_param(hdf5="testlog/simplesyst-dropreac.hdf5")
     res = syst.run()
     syst.log.info(f"Finished: {res}")
     syst.log.disconnect()
