7a5534afdf5ad9c9d37d2d9e626c9c3d8571caa6
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-02-13 16:25:06 +0100

    Simple launcher from json parameter file

---
+++ launchrun.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S python3 -O
+
+from argparse import ArgumentParser
+
+from metadynamic import System
+
+
+parser = ArgumentParser(description="Launch run from a json file")
+
+parser.add_argument("parameters", type=str, help="parameter json file")
+parser.add_argument("--log", metavar="logfile", type=str, nargs="?", help="log file")
+parser.add_argument(
+    "--level", metavar="loglevel", type=str, nargs="?", help="log level", default="INFO"
+)
+
+args = parser.parse_args()
+
+syst = System(args.parameters, logfile=args.log, loglevel=args.level)
+# syst.set_param(dropmode="drop")
+# syst.set_param(init={"a": 15000, "A": 30000}, maxsteps=100000)
+res = syst.run()
+
+print(res)
