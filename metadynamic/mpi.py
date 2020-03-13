385485d9ca1aa7de63bcbb98082411564a4fdb7f
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-03-13 15:37:55 +0100

    Allow redefining a gate operation.
    Necessary for ovrriding old operations!

--- metadynamic/mpi.py
+++ metadynamic/mpi.py
@@ -91,10 +91,9 @@ class MpiGate:
         self.exit()
 
     def register_function(self, opname: str, func: Callable[[], None]) -> None:
-        if opname not in self._opnum:
-            funcnum = len(self._op) + 1
-            self._op[funcnum] = func
-            self._opnum[opname] = funcnum
+        funcnum = len(self._op) + 1
+        self._op[funcnum] = func
+        self._opnum[opname] = funcnum
 
     def operate(self, funcnum: int) -> None:
         self._op[funcnum]()
