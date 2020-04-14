3fc6678846a7b186a3ed8fafa990a16dff5de5f8
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-04-14 08:19:40 +0200

    Docstring to inval

--- metadynamic/inval.py
+++ metadynamic/inval.py
@@ -13,50 +13,124 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+"""
+metadynamic.inval
+=================
+
+defines invalid values for any type. 
+
+Provides:
+---------
+
+ - L{InvalidError}: error to be raised when an invalid value is found where unexpected
+
+ - L{Invalid}: generic Invalid class
+
+ - L{isvalid}: function testing the validity of an object
+
+ - L{invalidint}: invalid int object.
+   isvalid(isvalidint) returns False; isinstance(invalidint, int) returns True
+
+ - L{invalidfloat}: invalid float object.
+   isvalid(isvalidfloat) returns False; isinstance(invalidfloat, float) returns True
+
+ - L{invalidstr}: invalid str object.
+   isvalid(isvalidstr) returns False; isinstance(invalidstr, str) returns True
+
+"""
 
 from typing import Any
 
 
 class InvalidError(ValueError):
+    """error to be raised when an invalid value is found where unexpected"""
+
     pass
 
 
 class Invalid:
+    """
+    For defining Invalid classes of any type.
+    An invalid object of type <T> is an instance of a class 
+    that inherits from both Invalid and <T>
+
+        >>> class InvalidInt(Invalid, int):
+                _invalrepr = "Invalid int"
+        >>> invalidint = InvalidInt()
+        >>> (invalidint, 3)
+        (Invalid int, 3)
+        >>> isvalid(invalidint), isvalid(3)
+        (False, True)
+        >>> isinstance(invalidint, int), isinstance(3,int)
+        (True, True)
+
+    Can be useful to define a default "no value"
+    parameter, that will be seen as an instance of a regular type
+
+    _invalrepr and _invalstr shall be overriden in subclasses
+    """
+
     _invalrepr = "Invalid Object"
+    """Representation of the invalid object"""
     _invalstr = ""
+    """String conversion of the invalid object"""
 
     def __bool__(self) -> bool:
+        """an invalid object is always False"""
         return False
 
     def __str__(self) -> str:
+        """
+        an invalid objects gets converted to self._invalstr.
+
+        defaults to empty string, shall be overriden in subclasses if necessary.
+        """
         return self._invalstr
 
     def __repr__(self) -> str:
+        """
+        an invalid objects gets represented by self._invalrepr.
+
+        defaults to 'Invalid Object', shall be overriden in subclasses.
+        """
         return self._invalrepr
 
 
 def isvalid(obj: Any) -> bool:
+    """Returns False only if the object is an instance of Invalid class
+
+    @param obj: object to be tested
+    @return: is the object valid?
+    @rtype: bool
+    """
     return not isinstance(obj, Invalid)
 
 
 class InvalidInt(Invalid, int):
+    """Invalid int class"""
+
     _invalrepr = "Invalid int"
 
 
-invalidint = InvalidInt()
+invalidint: InvalidInt = InvalidInt()
+"""invalint int object"""
 
 
 class InvalidFloat(Invalid, float):
+    """Invalit float class"""
+
     _invalrepr = "Invalid float"
 
 
-invalidfloat = InvalidFloat()
+invalidfloat: InvalidFloat = InvalidFloat()
+"""invalid float object"""
 
 
 class InvalidStr(Invalid, str):
+    """invalid str class"""
+
     _invalrepr = "Invalid string"
 
 
-invalidstr = InvalidStr()
-
-
+invalidstr: Invalidstr = InvalidStr()
+"""invalid str object"""
