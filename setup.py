e460d2fd045fa535a20fe982d8aa65803d4d3c50
Raphaël Plasson <raphael.plasson@univ-avignon.fr>
2020-04-16 17:37:45 +0200

    Modified bin/metarun for manpage generation from argparse-manpage + manpage creation

--- setup.py
+++ setup.py
@@ -5,40 +5,37 @@ import sys
 _here = os.path.abspath(os.path.dirname(__file__))
 
 if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'README.md')) as f:
+    with open(os.path.join(_here, "README.md")) as f:
         long_description = f.read()
 else:
-    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+    with open(os.path.join(_here, "README.md"), encoding="utf-8") as f:
         long_description = f.read()
 
 version = {}
-with open(os.path.join(_here, 'metadynamic', 'version.py')) as f:
+with open(os.path.join(_here, "metadynamic", "version.py")) as f:
     exec(f.read(), version)
 
 setuptools.setup(
-    name='metadynamic',
-    version=version['__version__'],
-    description=('Metadynamic Gillespie simulations with polymerization ruleset.'),
+    name="metadynamic",
+    version=version["__version__"],
+    description=("Metadynamic Gillespie simulations."),
     long_description=long_description,
-    author='Raphaël Plasson',
-    author_email='raphael.plasson@univ-avignon.fr',
+    author="Raphaël Plasson",
+    author_email="raphael.plasson@univ-avignon.fr",
     # url='https://github.com/bast/somepackage',
-    license='GPL V3.0',
+    license="GPL V3.0",
     packages=setuptools.find_packages(),
     # no dependencies in this example
-    install_requires=[
-        'setuptools>=33.1.1',
-        'numpy>=1.12.1'
-    ],
+    install_requires=["setuptools>=33.1.1", "numpy>=1.12.1"],
     # install_requires=[
     #       'dependency==1.2.3',
     # ],
     # no scripts in this example
-    scripts=['bin/metarun'],
+    scripts=["bin/metarun"],
     include_package_data=True,
     # classifiers=[
     #    'Development Status :: 5 - Production/Stable',
     #    'Intended Audience :: Science/Research',
     #    'Programming Language :: Python :: 2.7',
     #    'Programming Language :: Python :: 3.6'],
-    )
+)
